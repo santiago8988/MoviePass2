87e1bd7cb4a9880c3a19c7b491124ad1c7c1302c
Santiago <santiago.mdp@gmail.com>
2020-11-02 20:22:17 -0300

    akkaka

--- View/nav.php
+++ View/nav.php
@@ -1,15 +1,18 @@
 
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar-main">
+    <div class="logo">
 
-    <a href = "admin-view.php"><img src = "<?php echo IMG_PATH?>logo_page.png" alt = "logo_moviePass" class = "navbar-main_logo-page"></a>
-
-    <ol class = "navbar-main_ol">
-        <li>
-            <a href = "login.php">Iniciar Sesión</a>
-        <li>
-        <li>
-            <a href = "form-register.php">Registrarse</a>
-        </li>
-    </ol>
-</ol>
+            <a href = "admin-view.php"><img src ="<?php echo IMG_PATH?>logo_page.png" alt ="logo_moviePass" class = "navbar-main_logo-page"></a>
+        
+            
+     </div>
+        
+        <ol class = "navbar-main_ol">
+            <li>
+                <a href = "login.php">Iniciar Sesión</a>
+            <li>
+            <li>
+                <a href = "form-register.php">Registrarse</a>
+            </li>
+        </ol>
 </nav>
